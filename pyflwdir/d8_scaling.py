cdfc39a0fd5de7aed6b5c38f9c361734cf0056d4
DirkEilander <dirk.eilander@gmail.com>
2019-08-12 13:15:20 +0000

    implemented downstream functions

--- pyflwdir/d8_scaling.py
+++ pyflwdir/d8_scaling.py
@@ -5,10 +5,11 @@
 from numba import njit
 import numpy as np
 
-# set d8 format
-from d8_func import d8
-_pits = d8._pits
-_nodata = d8._nodata
+# import flow direction definition
+from .core import fd
+_nodata = fd._nodata
+_pits = fd._pits 
+_ds = fd._ds
 
 @njit
 def subidx_2_idx(subidx, sub_ncol, scale_ratio):
@@ -38,7 +39,7 @@ def _rep_cell(idx0, flwdir_flat, uparea_flat, shape, scale_ratio):
         for j in rr:
             idx += 1
             subidx = tilesubidx_2_subidx(idx, idx0, sub_ncol, scale_ratio)
-            ispit = d8.ispit(flwdir_flat[subidx])
+            ispit = fd.ispit(flwdir_flat[subidx])
             eff_area = (i**0.5 + j**0.5 <= R**0.5) or (i <= 0.5) or (j <= 0.5) # describes effective area
             if ispit or eff_area: 
                 upa = uparea_flat[subidx]
@@ -48,7 +49,7 @@ def _rep_cell(idx0, flwdir_flat, uparea_flat, shape, scale_ratio):
                     upa0 = upa
                     subidx0 = subidx
     
-    if subidx0 != -1 and d8.ispit(flwdir_flat[subidx0]) and np.all(np.array(repcells)!=subidx0):
+    if subidx0 != -1 and fd.ispit(flwdir_flat[subidx0]) and np.all(np.array(repcells)!=subidx0):
         repcells.append(subidx0)
     elif subidx0 == -1:
         # select center cell if no rep cell found
@@ -63,7 +64,7 @@ def _outlet(idx0, subidx, flwdir_flat, shape, scale_ratio):
     idx = idx0
     streamcells = [subidx]
     while True:
-        subidx_ds = d8.ds_d8(subidx, flwdir_flat, shape) # move downstream
+        subidx_ds = fd.ds_index(subidx, flwdir_flat, shape) # move downstream
         # outside domain or at pit/mouth
         if subidx_ds == -1 or subidx_ds == subidx: 
             if idx==idx0:
@@ -89,15 +90,12 @@ def _dd(idx0, subidx, flwdir_flat, effare_flat, shape, shape_lr, scale_ratio, ex
     dd = _pits[0]
     i = 0
     while True:
-        subidx_ds = d8.ds_d8(subidx, flwdir_flat, shape) # move downstream
+        subidx_ds = fd.ds_index(subidx, flwdir_flat, shape) # move downstream
         if subidx_ds == -1: # outside domain
             break 
         idx_ds = subidx_2_idx(subidx_ds, sub_ncol, scale_ratio)
         # at pit/mouth
-        if subidx_ds == subidx: 
-            # dd = d8.idx_to_d8(idx0, idx_ds, shape_lr)
-            # if dd == _pits[0]:
-            #     dd = flwdir_flat[subidx]
+        if subidx_ds == subidx:
             dd = flwdir_flat[subidx]
             break
         # center lowres cell
@@ -111,7 +109,7 @@ def _dd(idx0, subidx, flwdir_flat, effare_flat, shape, shape_lr, scale_ratio, ex
             # check if in subgrid hits outlet / eff area
             flag = effare_flat[subidx_ds] # 1 eff area; 2 outlet
             if  flag >= np.uint8(1):
-                dd = d8.idx_to_d8(idx0, idx_ds, shape_lr)
+                dd = fd.idx_to_dd(idx0, idx_ds, shape_lr)
                 assert dd != _nodata
                 if not extended or flag >= np.uint8(2):
                     break
@@ -128,23 +126,23 @@ def _ddplus(idx0, flwdir_lr_flat, outlet_lr_flat, flwdir_flat, uparea_flat, effa
     subidx_out = outlet_lr_flat[idx0]
     subidx_out_new = subidx_out
     # check if valid upstream & downstream cells
-    idxs_us = d8.us_d8(idx0, flwdir_lr_flat, shape_lr)
-    idx_ds = d8.ds_d8(idx0, flwdir_lr_flat, shape_lr)
+    idxs_us = fd.us_indices(idx0, flwdir_lr_flat, shape_lr)
+    idx_ds = fd.ds_index(idx0, flwdir_lr_flat, shape_lr)
     if idxs_us.size == 0 or np.any(idxs_us<0) or idx_ds == idx0 or idx_ds < 0:
         return idx0, _nodata, subidx_out
     # check if main us connects to next ds in d8
     subidxs_out_us = outlet_lr_flat[idxs_us]
     maxi = np.argmax(uparea_flat[subidxs_out_us])
     idx_us_main = idxs_us[maxi]
-    dd_new = d8.idx_to_d8(idx_us_main, idx_ds, shape_lr)
-    assert not d8.ispit(dd_new)
+    dd_new = fd.idx_to_dd(idx_us_main, idx_ds, shape_lr)
+    assert not fd.ispit(dd_new)
     if dd_new == _nodata: # -1 if it does not connect in d8
         return idx0, _nodata, subidx_out
     # trace main stream downstream
     subidx_us = subidxs_out_us[maxi]
     mainstream = [subidx_us]
     while True:
-        subidx_ds = d8.ds_d8(subidx_us, flwdir_flat, shape)
+        subidx_ds = fd.ds_index(subidx_us, flwdir_flat, shape)
         if subidx_ds == subidx_us or subidx_ds < 0:
             break
         mainstream.append(subidx_ds)
@@ -161,7 +159,7 @@ def _ddplus(idx0, flwdir_lr_flat, outlet_lr_flat, flwdir_flat, uparea_flat, effa
     while True and j < n-1:
         subidx_ds = mainstream[j]
         subidx_us_main = mainstream[j+1]
-        subidxs_us = d8.us_d8(subidx_ds, flwdir_flat, shape)
+        subidxs_us = fd.us_indices(subidx_ds, flwdir_flat, shape)
         for subidx_us in subidxs_us:
             if subidx_us == subidx_us_main:
                 continue
@@ -180,7 +178,7 @@ def _ddplus(idx0, flwdir_lr_flat, outlet_lr_flat, flwdir_flat, uparea_flat, effa
             if i == maxi: continue
             subidx_us = subidxs_out_us[i]
             while sound:
-                subidx_ds = d8.ds_d8(subidx_us, flwdir_flat, shape)
+                subidx_ds = fd.ds_index(subidx_us, flwdir_flat, shape)
                 invalid = subidx_ds == subidx_us or subidx_ds < 0 or effare_flat[subidx_ds] == np.uint8(3)
                 attrib = subidx_ds in tributaries
                 if attrib and (subidx_ds == subidx_out_new or subidx_out_new == subidx_out):
@@ -200,6 +198,29 @@ def _ddplus(idx0, flwdir_lr_flat, outlet_lr_flat, flwdir_flat, uparea_flat, effa
     return idx_us_main, dd_new, subidx_out_new
 
 @njit
+def subbasin_outlets_grid(scale_ratio, flwdir, uparea):
+    sub_nrow, sub_ncol = uparea.shape
+    lr_nrow, lr_ncol = int(sub_nrow/scale_ratio), int(sub_ncol/scale_ratio)
+    shape_lr = lr_nrow, lr_ncol
+    size_lr = lr_nrow * lr_ncol
+    # subgrid input
+    shape = flwdir.shape
+    flwdir_flat = flwdir.reshape(-1)
+    uparea_flat = uparea.reshape(-1)
+    # output cells
+    outlet_lr_flat = np.ones(size_lr, dtype=np.int64)*-1
+
+    # get representative cells (largest uparea in effective area) on highres maps
+    # trace to lowres cell boundary to find outlet cells
+    for idx0 in range(outlet_lr_flat.size):
+        subidx, _ = _rep_cell(idx0, flwdir_flat, uparea_flat, shape, scale_ratio)
+        subidx_out, _ = _outlet(idx0, subidx, flwdir_flat, shape, scale_ratio)
+        outlet_lr_flat[idx0] = subidx_out   # subgrid index of outlet point lowres grid cell
+
+    return outlet_lr_flat.reshape(shape_lr)
+
+
+@njit
 def d8_scaling(scale_ratio, flwdir, uparea, upa_min=0.5, extended=True):
     sub_nrow, sub_ncol = uparea.shape
     lr_nrow, lr_ncol = int(sub_nrow/scale_ratio), int(sub_ncol/scale_ratio)
@@ -207,13 +228,13 @@ def d8_scaling(scale_ratio, flwdir, uparea, upa_min=0.5, extended=True):
     size_lr = lr_nrow * lr_ncol
     # subgrid input
     shape = flwdir.shape
-    flwdir_flat = flwdir.flatten()
-    uparea_flat = uparea.flatten()
+    flwdir_flat = flwdir.reshape(-1)
+    uparea_flat = uparea.reshape(-1)
     # output cells
     outlet_lr_flat = np.ones(size_lr, dtype=np.int64)*-1
     repcel_lr_flat = np.ones(size_lr, dtype=np.int64)*-1
-    flwdir_lr_flat = np.zeros(shape_lr, dtype=flwdir.dtype).flatten()
-    effare_flat = np.zeros(shape, dtype=np.uint8).flatten()    
+    flwdir_lr_flat = np.zeros(size_lr, dtype=flwdir.dtype)
+    effare_flat = np.zeros(flwdir.size, dtype=np.uint8)    
 
     # get effective area and representative cells highres maps
     # and index of rep cell on lowres map
@@ -221,7 +242,6 @@ def d8_scaling(scale_ratio, flwdir, uparea, upa_min=0.5, extended=True):
         subidx_rep, subidxs_ea = _rep_cell(idx0, flwdir_flat, uparea_flat, shape, scale_ratio)
         repcel_lr_flat[idx0] = subidx_rep
         effare_flat[subidxs_ea] = np.uint(1) # save effective area
-    assert repcel_lr_flat.size == np.unique(repcel_lr_flat).size
 
     # find outlet cells and save to highres and lowres grids
     for idx0 in range(outlet_lr_flat.size):
@@ -255,133 +275,4 @@ def d8_scaling(scale_ratio, flwdir, uparea, upa_min=0.5, extended=True):
                 outlet_lr_flat[idx0] = subidx_out_new
     assert outlet_lr_flat.size == np.unique(outlet_lr_flat).size
     assert check_flwdir(flwdir_lr_flat, shape_lr).size==0
-
-    return flwdir_lr_flat.reshape(shape_lr), outlet_lr_flat.reshape(shape_lr), repcel_lr_flat.reshape(shape_lr)
-
-# TODO: check that all cells drain to an outlet.
-# On a tile level (with buffer) this could be done by checking 
-# that cells reach either the tile bounds or an outlet
-@njit
-def check_flwdir(flwdir_lr_flat, shape_lr):
-    nrow, ncol = shape_lr
-    idx_wrong = []
-    for idx0 in range(flwdir_lr_flat.size):
-        # skip edges 
-        r0 = idx0 // ncol
-        c0 = idx0 %  ncol
-        if r0 == 0 or r0 == nrow-1 or c0 == 0 or c0 == ncol-1:
-            continue
-        idx_ds = d8.ds_d8(idx0, flwdir_lr_flat, shape_lr)
-        if idx_ds == -1 or idx_ds == idx0:
-            continue
-        idx_ds2 = d8.ds_d8(idx_ds, flwdir_lr_flat, shape_lr)
-        if idx0 == idx_ds2:
-            idx_wrong.append(idx0)
-    return np.array(idx_wrong, dtype=np.int64)
-
-def get_upareas(flwdir_lr, outlet_lr, uparea, flwdir, scale_ratio):
-    from d8_flux import accuflux
-    from d8_scaling_subgrid import group_count, subgrid_indices
-    # get highres upgrid at outlet
-    mask_lr = flwdir_lr!=_nodata
-    subidx_out = outlet_lr[mask_lr]
-    upgrid_out = np.ones(mask_lr.shape, dtype=np.float32)*-9999
-    upgrid_out[mask_lr] = uparea.flat[subidx_out].astype(np.float32)
-    # caclute upgrid
-    idxs_ds = np.where(np.logical_or(flwdir_lr.flatten()==0, flwdir_lr.flatten()==255))[0]
-    print(idxs_ds)
-    if not idxs_ds.size == 1:
-        import pdb; pdb.set_trace()
-    outlet_lr = outlet_lr.copy()
-    outlet_lr[~mask_lr] = -9999
-    groups, indices, _ = subgrid_indices(outlet_lr, flwdir, uparea, upa_min=0.5)
-    catare = np.ones(mask_lr.shape, dtype=np.float32)
-    catare[mask_lr] = group_count(groups, indices).astype(np.float32)
-    upgrid_lr = accuflux(flwdir=flwdir_lr, material=catare, idxs_ds=idxs_ds).astype(np.float32)
-    upgrid_lr[~mask_lr] = -9999
-    return upgrid_out, upgrid_lr, mask_lr
-
-if __name__ == "__main__":
-    import xarray as xr
-    import pandas as pd
-    from os.path import join, isfile
-    import numpy as np
-    import matplotlib.pyplot as plt
-    # local libraries
-    from tif_io import open_vrt_dataset
-    from d8_scaling_performance import test_scaling
-
-    test = True
-    root = r'/media/data/hydro_merit_1.0'
-    # root = r'd:/work/flwdir_scaling'
-    merit_dir = join(root, r'03sec')
-    fn_outlets = join(merit_dir, f'all_outlets_upa100_pfaf.csv')
-    df = pd.read_csv(fn_outlets, index_col=0)
-    res = 1/1200.
-    buf = 0.25
-    
-    # select basin 
-    # WARNING: basin(s) 2941, 10599, 13074, 13666, 14549, 15276 have multiple outlets
-    idx = 102 #102 #931 #74: rhine
-    bas = df.loc[idx, ]
-    w,s,e,n = bas[['xmin', 'ymin', 'xmax', 'ymax']].values
-    w,s,e,n = w-buf, max(s-buf,-60), e+buf, min(n+buf,85.)
-    w,s,e,n = np.floor(w*4)/4.,np.floor(s*4)/4., np.ceil(e*4)/4., np.ceil(n*4)/4.
-    print(f'{idx:d}: ({w:.3f}, {s:.3f}, {e:.3f}, {n:.3f})')
-
-    test_fn = join(merit_dir, f'test_sel_idx{idx:d}.nc')
-    if test and isfile(test_fn):
-        ds = xr.open_dataset(join(merit_dir, f'test_sel_idx{idx:d}.nc')).load()
-    else:
-        ds = open_vrt_dataset(merit_dir, ['upa', 'dir', 'bas', 'upg'])
-    ds_sel = ds.sel(lon=slice(w, e), lat=slice(n, s))
-    mask = ds_sel['bas'].load().values != idx
-    flwdir = ds_sel['dir'].load().values
-    flwdir[mask] = np.uint8(247)
-    uparea = ds_sel['upa'].load().values
-    upgrid = ds_sel['upg'].load().values
-    height, width = uparea.shape
-    print(f'{idx:d}: ({height:d}x{width:d})')
-    if test and not isfile(test_fn):
-        ds_sel.to_netcdf(test_fn)
-
-    flwdir_flat = flwdir.flatten()
-    idxs_ds = np.where(np.logical_or(flwdir_flat==0, flwdir_flat==255))[0]
-    print(idxs_ds)
-    assert idxs_ds.size == 1
-    for scale_ratio in [20]: #[10, 20, 100, 300]:
-        out_res = scale_ratio*res
-        if 3600/(1/out_res) < 60:
-            resname = '{:02.0f}sec'.format(3600/(1/out_res))
-        else:
-            resname = '{:02.0f}min'.format(60/(1/out_res))
-        print(f'out res = {resname:s} ({height/scale_ratio:.0f}, {width/scale_ratio:.0f})')
-
-        # flwdir_lr, outlet_lr, _ = d8_scaling(scale_ratio, flwdir, uparea, extended=False)
-        # upgrid_out, upgrid_lr, mask_lr = get_upareas(flwdir_lr, outlet_lr, upgrid, flwdir, scale_ratio)
-        # nse = test_scaling(upgrid_lr[mask_lr], upgrid_out[mask_lr])
-        # print(f'EEA original nse: {nse:.4f}')
-
-        flwdir_lr_ext, outlet_lr_ext, _ = d8_scaling(scale_ratio, flwdir, uparea, extended=True)
-        upgrid_out_ext, upgrid_lr_ext, mask_lr_ext = get_upareas(flwdir_lr_ext, outlet_lr_ext, upgrid, flwdir, scale_ratio)
-        nse_ext = test_scaling(upgrid_lr_ext[mask_lr_ext], upgrid_out_ext[mask_lr_ext])
-        print(f'EEA ext nse: {nse_ext:.4f}')
-
-        # fig, (ax1, ax2) = plt.subplots(1,2, figsize=(10,5), sharey=True)
-        # ax1.set_aspect('equal')
-        # ax2.set_aspect('equal')
-        # ax1.set_yscale('log')
-        # ax1.set_xscale('log')
-        # ax2.set_yscale('log')
-        # ax2.set_xscale('log')
-        # ax1.scatter(upgrid_lr, upgrid_out, color='k', s=6, marker='o', label=f'original; NSE: {nse:.4f}')
-        # ax2.scatter(upgrid_lr_ext, upgrid_out_ext, color='r', s=6, marker='*', label=f'extended; NSE: {nse_ext:.4f}')
-        # ax2.legend(loc='lower right')
-        # ax1.legend(loc='lower right')
-        # ax1.set_xlabel('upgrid at outlet [no. cells]')
-        # ax2.set_xlabel('upgrid at outlet [no. cells]')
-        # ax1.set_ylabel('scaled upgrid [no. cells]')
-        # ax1.set_title(f'EEA original: res: {resname}')
-        # ax2.set_title(f'EEA extended: res: {resname}')
-        # fn = join(merit_dir, '../plots', f'basin{idx:03d}_{resname}.png')
-        # plt.savefig(fn, dpi=255, bbox_axis='tight')
+    return flwdir_lr_flat.reshape(shape_lr), outlet_lr_flat.reshape(shape_lr)
--- pyflwdir/pyflwdir.py
+++ pyflwdir/pyflwdir.py
@@ -10,7 +10,7 @@ from rasterio.transform import Affine, array_bounds
 
 # local
 from .core import fd
-from . import flux, utils, features, network, gridtools
+from . import flux, utils, features, network, gridtools, d8_scaling
 # export
 __all__ = ['FlwdirRaster']
 
@@ -41,82 +41,103 @@ class FlwdirRaster(object):
         self.crs = rasterio.crs.CRS.from_user_input(crs)
         self.latlon = self.crs.is_geographic # NOTE: we assume this is in latlon
         self.transform = transform
+        self.res = transform[0], transform[4]
+        self.cellare = np.abs(transform[0]*transform[4])
         self.shape = data.shape
         self.bounds = array_bounds(data.shape[0], data.shape[1], transform)
         self.size = data.size
         if self.size > 2**31-1:
             raise ValueError('Extent too large for int32 indices')
         if create_copy:
-            self.data = data.copy()
+            self._data = data.copy()
         else:
-            self.data = data.view() # view of data
-        self.data_flat = self.data.ravel() # flattened view of data
-        if check_format and fd._check_format(self.data) == False: # simple check. isvalid includes more rigorous check
+            self._data = data.view() # view of data
+        self._data_flat = self._data.reshape(-1) # flattened view of data
+        if check_format and fd._check_format(self._data) == False: # simple check. isvalid includes more rigorous check
             raise ValueError('Unknown flow direction values found in data')
         # set placeholder properties
-        self.idx0 = None            # most downstream indices in network
-        self.rnodes = None          # network ds nodes (n)
-        self.rnodes_up = None       # network us nodes (n,m); m <= 8 in d8
-        self.rbasins = None         # network ds basin id (n)
+        self._idx0 = None            # most downstream indices in network
+        self._rnodes = None          # network ds nodes (n)
+        self._rnodes_up = None       # network us nodes (n,m); m <= 8 in d8
 
     def __getitem__(self, key):
-        return self.data_flat[key]
+        return self._data_flat[key]
 
     def __setitem__(self, key, item):
-        self.data_flat[key] = item
+        self._data_flat[key] = item
+
+    # def __del__(self):
+    #     del self._rnodes[:]
+    #     del self._rnodes
+    #     del self._rnodes_up[:]
+    #     del self._rnodes_up
 
     def _xycoords(self):
-        res = self.transform[0]
+        resx, resy = self.res
         xmin, ymin, xmax, ymax = self.bounds
-        ys = np.linspace(ymax-res/2., ymin+res/2., self.shape[0])
-        xs = np.linspace(xmin+res/2., xmax-res/2., self.shape[1])
+        if resy < 0:
+            ys = np.linspace(ymax+resy/2., ymin-resy/2., self.shape[0])
+        else:
+            ys = np.linspace(ymin+resy/2., ymax-resy/2., self.shape[0])
+        xs = np.linspace(xmin+resx/2., xmax-resx/2., self.shape[1])
         return xs, ys
 
     def isvalid(self):
         # check if all cells connect to pit / outflows at bounds
-        valid = utils.flwdir_check(self.data)[1] == False
+        valid = utils.flwdir_check(self._data)[1] == False
         return valid
 
     def repair(self):
-        repair_idx, _ = utils.flwdir_check(self.data)
+        repair_idx, _ = utils.flwdir_check(self._data)
         if repair_idx.size > 0:
-            self.data_flat[repair_idx] = self._pits[-1] # set inland pit
+            self._data_flat[repair_idx] = self._pits[-1] # set inland pit
+        return None
 
     def setup_network(self, idx0=None):
         if idx0 is None:
             idx0 = self.get_pits()
             if idx0.size == 0:
                 raise ValueError('no pits found in flow direction data')       
-        self.idx0 = np.atleast_1d(np.asarray(idx0, dtype=np.int32)) # basin outlets
-        self.rnodes, self.rnodes_up = network.setup_dd2(self.idx0, self.data_flat, self.shape)
+        self._idx0 = np.atleast_1d(np.asarray(idx0, dtype=np.int32)) # basin outlets
+        self._rnodes, self._rnodes_up = network.setup_dd(self._idx0, self._data_flat, self.shape)
+        return None
 
     def get_pits(self):
-        return fd.pit_indices(self.data.flatten())
+        return fd.pit_indices(self._data.flatten())
 
     def upstream_area(self, cell_area=None):
         """returns upstream area in km"""
-        if self.rnodes is None:
+        if self._rnodes is None:
             self.setup_network()    # setup network, with pits as most downstream indices
-        if cell_area is None and self.latlon:
-            cell_area = gridtools.latlon_cellare_metres(self.transform, self.shape)/1e6
-        elif cell_area is None:
-            cell_area = gridtools.cellare_metres(self.transform, self.shape)/1e6
-        if not (np.all(cell_area.shape == self.shape)):
-            raise ValueError(f"cell_area shape {cell_area.shape} does not match flwdir shape {self.shape}")
-        return flux.propagate_downstream(self.rnodes, self.rnodes_up, material=cell_area)
+        # if cell_area is None and self.latlon:
+        #     cell_area = gridtools.latlon_cellare_metres(self.transform, self.shape)/1e6
+        # elif cell_area is None:
+        #     cell_area = gridtools.cellare_metres(self.transform, self.shape)/1e6
+        # if not (np.all(cell_area.shape == self.shape)):
+        #     raise ValueError(f"cell_area shape {cell_area.shape} does not match flwdir shape {self.shape}")
+        # return flux.propagate_downstream(self._rnodes, self._rnodes_up, material=cell_area)
+        if self.latlon:
+            _, ys = self._xycoords()
+            cellare = gridtools.lat_to_area(ys)*self.cellare
+        else:
+            cellare = np.ones(self.shape[0])*self.cellare
+        return network.upstream_area(self._rnodes, self._rnodes_up, cellare/1e6, self.shape)
 
-    # def basin_bounds(self):
-    #     res = self.transform[0]
-    #     xs, ys = self._xycoords()
-    #     if self.rnodes is None:
-    #         self.setup_network()    # setup network, with pits as most downstream indices
-    #     return features.basin_bbox(self.rnodes, self.rnodes_up, self.rbasins, ys, xs, res)
-
-    def basin_map(self, idx=None, values=None, dtype=np.int32):
-        if self.rnodes is None:
+    def basin_bounds(self, idx=None):
+        resx, resy = self.res
+        xs, ys = self._xycoords()
+        if self._rnodes is None:
             self.setup_network()    # setup network, with pits as most downstream indices
         if idx is None:             # use most downstream network indices if idx not given
-            idx = self.idx0
+            idx = self._idx0
+        idx = np.atleast_1d(idx).astype(np.int32)
+        return features.basin_bbox(self._rnodes, self._rnodes_up, idx, ys, xs, resy, resx)
+
+    def basin_map(self, idx=None, values=None, dtype=np.int32):
+        if self._rnodes is None:
+            self.setup_network()    # setup network, with pits as most downstream indices
+        if idx is None:             # use most downstream network indices if idx not given
+            idx = self._idx0
         idx = np.atleast_1d(idx).astype(np.int32)
         if values is None:          # number basins using range, starting from 1
             values = np.arange(idx.size, dtype=dtype)+1
@@ -126,17 +147,31 @@ class FlwdirRaster(object):
                 raise ValueError('all values should be larger than zero')
         if not idx.size == values.size and idx.ndim == 1:
             raise ValueError('idx and values should be 1d arrays of same size')
-        return network.delineate_basins(self.rnodes, self.rnodes_up, idx, values, self.shape)
+        return network.delineate_basins(self._rnodes, self._rnodes_up, idx, values, self.shape)
 
-    def basin_shape(self, basin_map=None, **kwargs):
+    def subbasin_map_grid(self, scale_ratio, uparea=None):
+        if not self.shape[0] % scale_ratio == self.shape[1] % scale_ratio == 0:
+            raise ValueError(f'the data shape should be an exact multiplicity of the scale_ratio')
+        if uparea == None:
+            uparea = self.upstream_area()
+        subbasin_idx = d8_scaling.subbasin_outlets_grid(scale_ratio, self._data, uparea)
+        subbasin_idx = subbasin_idx[subbasin_idx!=-9999]
+        return self.basin_map(idx=subbasin_idx, values=None, dtype=np.int32)
+
+    def subbasin_map_pfaf(self):
+        # TODO create pfafstetter subbasins
+        raise NotImplementedError()
+
+    def basin_shape(self, basin_map=None, nodata=0, **kwargs):
         if basin_map is None:
             basin_map = self.basin_map(**kwargs)
-        return gridtools.vectorize(basin_map, 0, self.transform, crs=self.crs)
+            nodata = 0 # overwrite nodata
+        return gridtools.vectorize(basin_map, nodata, self.transform, crs=self.crs)
 
     def stream_order(self):
-        if self.rnodes is None:
+        if self._rnodes is None:
             self.setup_network()    # setup network, with pits as most downstream indices
-        return network.stream_order(self.rnodes, self.rnodes_up, self.shape).astype(np.int16)
+        return network.stream_order(self._rnodes, self._rnodes_up, self.shape)
 
     def stream_shape(self, stream_order=None, mask=None, min_order=3):
         if stream_order is None:
@@ -145,8 +180,21 @@ class FlwdirRaster(object):
             stream_order[stream_order < min_order] = np.int16(-1)
         else:
             stream_order[mask] = np.int16(-1)
-        riv_nodes, riv_order = features.river_nodes(self.idx0, self.data_flat, stream_order.ravel(), self.shape)
+        riv_nodes, riv_order = features.river_nodes(self._idx0, self._data_flat, stream_order.reshape(-1), self.shape)
         xs, ys = self._xycoords()
         geoms = [gridtools.nodes_to_ls(nodes, ys, xs, self.shape) for nodes in riv_nodes]
         return gridtools.gp.GeoDataFrame(data=riv_order, columns=['stream_order'], geometry=geoms, crs=self.crs)
         
+    def upscale(self, scale_ratio, uparea=None, upa_min=0.5, method='extended', return_outlets=False):
+        if not self.shape[0] % scale_ratio == self.shape[1] % scale_ratio == 0:
+            raise ValueError(f'the data shape should be an exact multiplicity of the scale_ratio')
+        if uparea == None:
+            uparea = self.upstream_area()
+        transform_lr = self.transform.copy()
+        transform_lr[0], transform_lr[4] = self.transform[0] * scale_ratio, self.transform[4] * scale_ratio
+        flwdir_lr, outlet_lr = d8_scaling(scale_ratio, self._data, uparea, upa_min=upa_min, extended=method=='extended')
+        flwdir_lr = FlwdirRaster(flwdir_lr, transform=transform_lr, crs=self.crs)
+        return flwdir_lr, outlet_lr
+
+
+
