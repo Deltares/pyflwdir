23b6610b4152cc1b448077a233949ad301c55356
DirkEilander <dirk.eilander@gmail.com>
2020-02-25 18:27:14 +0100

    removed unnecessary lines in stream order methods

--- pyflwdir/basin.py
+++ pyflwdir/basin.py
@@ -99,7 +99,6 @@ def basins(tree, idxs_us, idxs_pit):
 @njit
 def _strahler_order(idxs_us, strord_flat):
     """"""
-    head_lst = list()
     ord_max = np.int8(1)
     ord_cnt = 0
     for i in range(idxs_us.size):
@@ -107,9 +106,6 @@ def _strahler_order(idxs_us, strord_flat):
         if idx_us == _mv:
             break
         ordi = strord_flat[idx_us]
-        if ordi <= 0:  # most upstream cells
-            ordi = np.int8(1)
-            head_lst.append(idx_us)
         if ordi >= ord_max:
             if ordi == ord_max:
                 ord_cnt += 1
@@ -119,7 +115,7 @@ def _strahler_order(idxs_us, strord_flat):
     # where two channels of order i join, a channel of order i+1 results
     if ord_cnt >= 2:
         ord_max += 1
-    return ord_max, np.array(head_lst, dtype=np.uint32)
+    return ord_max
 
 
 @njit
@@ -131,10 +127,8 @@ def stream_order(tree, idxs_us):
         idxs = tree[-i - 1]  # from up- to downstream
         for idx0 in idxs:
             idxs_us0 = idxs_us[idx0, :]
-            ordi, idx_head = _strahler_order(idxs_us0, strord_flat)
+            ordi = _strahler_order(idxs_us0, strord_flat)
             if ordi > 127:
                 raise TypeError("maximum stream order is 127")
             strord_flat[idx0] = np.int8(ordi)  # update stream order downstream cells
-            if idx_head.size > 0:  # update head cells
-                strord_flat[idx_head] = np.int8(1)
     return strord_flat
