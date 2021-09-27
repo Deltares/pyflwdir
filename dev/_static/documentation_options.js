6b212572c3ff6306bfcd71811b4819c6c86140f4
DirkEilander <DirkEilander@users.noreply.github.com>
2021-09-27 06:12:05 +0000

    Deploy dev to GitHub Pages

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g5c5d362',
+    VERSION: '0.1.dev1+g9a86742',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
