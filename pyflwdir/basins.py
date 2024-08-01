fix overflow errors numpy v2

pyflwdir/basins.py
@@ -89,11 +89,11 @@ def subbasins_streamorder(idxs_ds, seq, strord, mask=None, min_sto=-2):
         map with unique IDs for stream_order>=min_sto subbasins
     """
     if min_sto < 0:
-        min_sto = strord.max() + min_sto
+        min_sto = int(strord.max()) + min_sto
     subbas = np.full(idxs_ds.shape, 0, dtype=np.int32)
     idxs = []
     for idx0 in seq[::-1]:  # up- to downstream
-        if (mask is not None and mask[idx0] == False) or strord[idx0] < min_sto:
+        if (mask is not None and mask[idx0] is False) or strord[idx0] < min_sto:
             continue
         idx_ds = idxs_ds[idx0]
         if strord[idx0] != strord[idx_ds] or idx_ds == idx0:

pyflwdir/core_d8.py
@@ -2,8 +2,10 @@
 """Description of D8 flow direction type and methods to convert to/from general
 nextidx."""
 
-from numba import njit, vectorize
+from typing import Tuple
 import numpy as np
+from numba import njit
+
 from . import core
 
 __all__ = []
@@ -50,8 +52,8 @@ def from_array(flwdir, _mv=_mv, dtype=np.intp):
         if flwdir_flat[idx0] == _mv:
             continue
         dr, dc = drdc(flwdir_flat[idx0])
-        r_ds = int(idx0 // ncol + dr)
-        c_ds = int(idx0 % ncol + dc)
+        r_ds = int(idx0 // ncol) + int(dr)
+        c_ds = int(idx0 % ncol) + int(dc)
         pit = dr == 0 and dc == 0
         outside = r_ds >= nrow or c_ds >= ncol or r_ds < 0 or c_ds < 0
         idx_ds = c_ds + r_ds * ncol
@@ -82,7 +84,7 @@ def _downstream_idx(idx0, flwdir_flat, shape, mv=core._mv):
 
 # general
 @njit
-def to_array(idxs_ds, shape, mv=core._mv):
+def to_array(idxs_ds: np.ndarray[np.uint64], shape: Tuple[int, int], mv=core._mv):
     """convert downstream linear indices to dense D8 raster"""
     ncol = shape[1]
     flwdir = np.full(idxs_ds.size, _mv, dtype=np.uint8)
@@ -90,17 +92,17 @@ def to_array(idxs_ds, shape, mv=core._mv):
         idx_ds = idxs_ds[idx0]
         if idx_ds == mv:
             continue
-        dr = (idx_ds // ncol) - (idx0 // ncol)
-        dc = (idx_ds % ncol) - (idx0 % ncol)
+        dr: np.int32 = np.int32(idx_ds // ncol) - np.int32(idx0 // ncol)
+        dc: np.int32 = np.int32(idx_ds % ncol) - np.int32(idx0 % ncol)
         if dr >= -1 and dr <= 1 and dc >= -1 and dc <= 1:
-            dd = _ds[dr + 1, dc + 1]
+            dd: np.uint8 = _ds[dr + 1, dc + 1]
         else:
-            raise ValueError("Invalid data downstream index outside 8 neighbours.")
+            raise ValueError("Invalid data downstream index outside 8 neighbors.")
         flwdir[idx0] = dd
     return flwdir.reshape(shape)
 
 
-def isvalid(flwdir, _all=_all):
+def isvalid(flwdir: np.uint8, _all: np.ndarray[np.uint8] = _all) -> bool:
     """True if 2D D8 raster is valid"""
     return (
         isinstance(flwdir, np.ndarray)

pyflwdir/flwdir.py
@@ -22,20 +22,44 @@ __all__ = ["Flwdir", "from_dataframe"]
 logger = logging.getLogger(__name__)
 
 
-@njit
-def get_lin_indices(idxs, idxs_ds):
-    idxs_ds0 = np.arange(idxs.size, dtype=idxs.dtype)
-    for j, idx_ds in enumerate(idxs_ds):
-        for i, idx in enumerate(idxs):
-            if idx == idx_ds:
-                idxs_ds0[j] = i
-    return idxs_ds0
+# @njit(cache=True)
+def get_loc_idx(idxs: np.ndarray, idxs_ds: np.ndarray) -> np.ndarray:
+    """Get location indices of downstream cells for all downstream cells"""
+    max_val = idxs.max()
+    idx_map = np.empty(max_val + 1, dtype=np.int_)
+    idx_map.fill(-1)  # Fill with -1 to indicate missing values
+    idx_map[idxs] = np.arange(idxs.size)
+    return idx_map[idxs_ds]
 
 
-def from_dataframe(df, ds_col="idx_ds"):
+def from_dataframe(df: "pandas.DataFrame", ds_col="idx_ds") -> "Flwdir":
+    """Create a Flwdir object from a dataframe with flow direction data.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        dataframe with flow directions
+    ds_col : str, optional
+        name of column with downstream indices, by default "idx_ds"
+
+    Returns
+    -------
+    Flwdir
+        flow direction object
+    """
+
     idxs_ds = df[ds_col].values
     idxs = df.index.values
-    return Flwdir(idxs_ds=get_lin_indices(idxs=idxs, idxs_ds=idxs_ds))
+    return Flwdir(idxs_ds=get_loc_idx(idxs=idxs, idxs_ds=idxs_ds))
+
+
+# def _get_idxs_ds_upstream(idxs: np.ndarray, idxs_up: np.ndarray) -> np.ndarray:
+#     idxs_ds0 = np.arange(idxs.size, dtype=idxs.dtype)
+#     for j, idx_up in enumerate(idxs_up):
+#         for i, idx in enumerate(idxs):
+#             if idx == idx_up:
+#                 idxs_up0[j] = i
+#     return idxs_ds0
 
 
 class Flwdir(object):
@@ -95,6 +119,8 @@ class Flwdir(object):
         # check validity
         if self.idxs_pit.size == 0:
             raise ValueError("Invalid FlwdirRaster: no pits found")
+
+    ### REPRESENTATION ###
 
     def __str__(self):
         return pprint.pformat(self._dict)

pyflwdir/upscale.py
@@ -22,16 +22,16 @@ __all__ = []
 @njit
 def subidx_2_idx(subidx, subncol, cellsize, ncol):
     """Returns the lowres index <idx> of highres cell index <subidx>."""
-    r = (subidx // subncol) // cellsize
-    c = (subidx % subncol) // cellsize
+    r = int(subidx // subncol) // cellsize
+    c = int(subidx % subncol) // cellsize
     return r * ncol + c
 
 
 @njit
 def in_d8(idx0, idx_ds, ncol):
     """Returns True if inside 3x3 (current and 8 neighboring) cells."""
-    cond1 = abs(idx_ds % ncol - idx0 % ncol) <= 1  # west - east
-    cond2 = abs(idx_ds // ncol - idx0 // ncol) <= 1  # south - north
+    cond1 = abs(int(idx_ds % ncol) - int(idx0 % ncol)) <= 1  # west - east
+    cond2 = abs(int(idx_ds // ncol) - int(idx0 // ncol)) <= 1  # south - north
     return cond1 and cond2
 
 
@@ -1068,8 +1068,8 @@ def ihu_minimize_error(
         check_pit = pit_out_of_cell > 0 and subidx_ds == subidx
         if check_pit:
             idx1 = subidx_2_idx(subidx_ds, subncol, cellsize, ncol)
-            dr = (idx1 % ncol) - (idx0 % ncol)
-            dc = (idx1 // ncol) - (idx0 // ncol)
+            dr = int(idx1 % ncol) - int(idx0 % ncol)
+            dc = int(idx1 // ncol) - int(idx0 // ncol)
             check_pit = abs(dr) <= pit_out_of_cell and abs(dc) <= pit_out_of_cell
         # not outlet cells and at pit -> reset outlet to pit
         if check_pit and (subidx_ds == subidx0 or len(idxs) == 0):
@@ -1359,7 +1359,7 @@ def upscale_error(subidxs_out, idxs_ds, subidxs_ds, mv=_mv):
                 # next iter
                 subidx = subidx1
         else:
-            connect_map[idx0] = np.uint8(-1)
+            connect_map[idx0] = np.uint8(255)  # -1
     return connect_map, np.array(idxs_fix_lst, dtype=idxs_ds.dtype)
 
 

tests/test_core.py
@@ -3,42 +3,16 @@
 
 import pytest
 import numpy as np
-import os
 
-from pyflwdir import core, core_d8, streams, from_dem
-
-# _mv = core._mv
-
-# # test data
-# # slice from real data
-# testdir = os.path.dirname(__file__)
-# flwdir0 = np.loadtxt(os.path.join(testdir, "flwdir.asc"), dtype=np.uint8)
-# idxs_ds0, idxs_pit0, _ = core_d8.from_array(flwdir0, dtype=np.uint32)
-# rank0, n0 = core.rank(idxs_ds0, mv=np.uint32(_mv))
-# seq0 = np.argsort(rank0)[-n0:]
-# parsed0 = (idxs_ds0, idxs_pit0, seq0, rank0, np.uint32(_mv))
-# # random data
-# # np.random.seed(2345)
-# flwdir1 = from_dem(np.random.rand(15, 10)).to_array("d8")
-# idxs_ds1, idxs_pit1, _ = core_d8.from_array(flwdir1, dtype=np.uint32)
-# rank1, n1 = core.rank(idxs_ds1, mv=np.uint32(_mv))
-# seq1 = np.argsort(rank1)[-n1:]
-# parsed1 = (idxs_ds1, idxs_pit1, seq1, rank1, np.uint32(_mv))
-# # combined
-# test_data = [(parsed0, flwdir0), (parsed1, flwdir1)]
+from pyflwdir import core, streams
 
 
-def test_downstream0(test_data0, flwdir0):
-    _test_downstream(test_data0, flwdir0)
-
-
-def test_downstream1(test_data1, flwdir1):
-    _test_downstream(test_data1, flwdir1)
-
-
-# @pytest.mark.parametrize("parsed, flwdir", test_data)
-# def test_downstream(test_data0, flwdir0):
-def _test_downstream(test_data, flwdir):
+@pytest.mark.parametrize(
+    "test_data, flwdir", [("test_data0", "flwdir0"), ("test_data0", "flwdir0")]
+)
+def test_downstream(test_data, flwdir, request):
+    test_data = request.getfixturevalue(test_data)
+    flwdir = request.getfixturevalue(flwdir)
     idxs_ds, idxs_pit, seq, rank, mv = [p.copy() for p in test_data]
     n, ncol = np.sum(idxs_ds != mv), flwdir.shape[1]
     # rank
@@ -86,17 +60,13 @@ def _test_downstream(test_data, flwdir):
         assert np.all(rank3[idxs1] == n_up)
 
 
-def test_upstream0(test_data0, flwdir0):
-    _test_upstream(test_data0, flwdir0)
-
-
-def test_upstream1(test_data1, flwdir1):
-    _test_upstream(test_data1, flwdir1)
-
-
-# @pytest.mark.parametrize("parsed, flwdir", test_data)
-def _test_upstream(parsed, flwdir):
-    idxs_ds, idxs_pit, seq, rank, mv = [p.copy() for p in parsed]
+@pytest.mark.parametrize(
+    "test_data, flwdir", [("test_data0", "flwdir0"), ("test_data0", "flwdir0")]
+)
+def test_upstream(test_data, flwdir, request):
+    test_data = request.getfixturevalue(test_data)
+    flwdir = request.getfixturevalue(flwdir)
+    idxs_ds, idxs_pit, seq, rank, mv = [p.copy() for p in test_data]
     idxs_ds[rank == -1] = mv
     n, ncol = np.sum(idxs_ds != mv), flwdir.shape[1]
     upa = streams.upstream_area(idxs_ds, seq, ncol, dtype=np.int32)