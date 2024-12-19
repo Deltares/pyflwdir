fix overflow errors numpy v2 (#51)
* fix overflow errors numpy v2

* fix stream_order bug

* update contributing.rst

* add pixi.lock file

tests/test_core_xx.py
@@ -11,10 +11,9 @@ from pyflwdir.core_conversion import ldd_to_d8, d8_to_ldd
 @pytest.mark.parametrize("fd", [core_nextxy, core_d8, core_ldd])
 def test_core(fd):
     """test core_x.py submodules based on _us definitions"""
-    _name = fd._ftype
     # test isvalid
     assert fd.isvalid(fd._us)
-    assert not fd.isvalid(fd._us * -1)
+    assert not fd.isvalid(fd._us * 3)
     # test ispit
     assert np.all(fd.ispit(fd._pv))
     # test isnodata

tests/test_pyflwdir.py
@@ -8,6 +8,7 @@ from affine import Affine
 
 import pyflwdir
 from pyflwdir import core
+from pyflwdir.pyflwdir import FlwdirRaster
 
 
 @pytest.mark.parametrize("flwdir, ftype", [("flwdir0", "d8"), ("nextxy0", "nextxy")])
@@ -254,7 +255,7 @@ def test_streams(flw0, flwdir0_rank):
     # check agains Flwdir
     # FIXME this fails, but only locally ??!#
     findex_ds = np.array([f["properties"]["idx_ds"] for f in feats])
-    flw1 = pyflwdir.Flwdir(pyflwdir.flwdir.get_lin_indices(findex, findex_ds))
+    flw1 = pyflwdir.Flwdir(pyflwdir.flwdir.get_loc_idx(findex, findex_ds))
     assert np.all(fstrord == flw1.stream_order().ravel())
     # vectorize
     feats = flw0.vectorize()
@@ -305,7 +306,7 @@ def test_upscale(flw0, nextxy0):
         flw0.upscale(5, uparea=np.ones((2, 1)))
 
 
-def test_ucat(flw0):
+def test_ucat(flw0: FlwdirRaster):
     elevtn = flw0.rank
     hand = flw0.hand(elevtn=elevtn, drain=elevtn == 0)
     depths = np.linspace(0.5, 1, 2)